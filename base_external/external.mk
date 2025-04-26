231c7a2f38edd8076f47f89b35ad035229c374e3
curtishunt <curtis.hunt17@gmail.com>
2025-04-26 11:35:22 -1000

    changed external.mk_0

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,4 +1,1 @@
-#includes make file in aesd assignments
-include $BR2_EXTERNAL_project_base_PATH/package/aesd-assignments/aesd-assignments.mk
-#includes all make files in packages
-#include $(sort $(wildcard $(BR2_EXTERNAL_PROJECT_BASE_PATH)/package/*/*.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_project_base_PATH)/package/*/*.mk))
