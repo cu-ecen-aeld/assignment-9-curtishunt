31149c80b04cba6d63b16337416eeb59171dca4d
Curtis Hunt <chunt@oceanit.com>
2025-04-26 23:00:06 -1000

    updated post build

--- base_external/board/qemu/post-build.sh
+++ base_external/board/qemu/post-build.sh
@@ -3,6 +3,6 @@
 
 TARGET_DIR=$1
 mkdir -p ${TARGET_DIR}/root/.ssh
-cp board/ssh/id_rsa_aeld.pub ${TARGET_DIR}/root/.ssh/authorized_keys
+cp board/ssh/id_ed25519.pub ${TARGET_DIR}/root/.ssh/authorized_keys
 chmod 700 ${TARGET_DIR}/root/.ssh
 chmod 600 ${TARGET_DIR}/root/.ssh/authorized_keys
